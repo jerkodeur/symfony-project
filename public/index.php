d41af15e3d67bf51a77bf7f78752b30533d8b04b
jerkoLegion <jerome.potie@gmail.com>
2021-09-10 14:50:32 +0200

    Install and use symfony routing in our application

--- public/index.php
+++ public/index.php
@@ -2,26 +2,32 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
-$response = new Response();
 
-$map = [
-    '/hello' => 'pages/',
-    '/bye' => 'pages/',
-    '/about' => 'pages/about'
-];
+$routes = require('../src/routes.php');
 
-$path = $request->getPathInfo();
+// Get the current http request infos needed by the symfony urlMatcher
+$context = new RequestContext();
+$context->fromRequest($request);
 
-if (in_array($path, array_keys($map))) {
+$urlMatcher = new UrlMatcher($routes, $context);
+
+try {
+    extract($urlMatcher->match($request->getPathInfo()));
+
     ob_start();
-    include __DIR__ . '/../src/' . $map[$path] . $path . '.php';
-    $response->setContent(ob_get_clean());
-} else {
-    $response->setStatusCode(404)->setContent('Page not found');
+    include __DIR__ . '/../src/pages/' . $_route . '.php';
+    $response = new Response(ob_get_clean());
+} catch (ResourceNotFoundException $e) {
+    $response = new Response('La page recherchée n\'a pas été trouvée !', 404);
+} catch (Exception $e) {
+    $response = new Response('Une erreur est survenue au niveau du serveur', 500);
 }
 
 $response->send();
--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,5 +1,1 @@
-<?php
-$name = $request->query->get('name', 'word');
-?>
-
 <h1>Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?></h1>
